c5f26d4cb4c4d3fbbf779a13617ce4f728f5f34e
Ferdinand Malcher <ferdinand@malcher.media>
2025-12-08 14:33:42 +0100

    BookCard und property Binding

---
+++ book-rating/src/app/books/book-card/book-card.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'app-book-card',
+  imports: [],
+  templateUrl: './book-card.html',
+  styleUrl: './book-card.scss',
+})
+export class BookCard {
+  // Input: hier fließen Daten von der Elternkomponente hinein
+  // von oben nach unten
+  // readonly book = input<number>();
+  // readonly bookx = input<number>(5);
+  readonly book = input.required<Book>();
+}
