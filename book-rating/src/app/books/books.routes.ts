e0fe84df2a12de7f2183687d8a89b4b0ade3b7a5
Ferdinand Malcher <ferdinand@malcher.media>
2025-12-10 14:35:21 +0100

    Formularseite und Navigation

--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { DashboardPage } from "./dashboard-page/dashboard-page";
 import { BookDetailsPage } from "./book-details-page/book-details-page";
+import { BookCreatePage } from "./book-create-page/book-create-page";
 
 export const booksRoutes: Routes = [
   { path: 'books', component: DashboardPage, title: 'Dashboard' },
-  { path: 'books/:isbn', component: BookDetailsPage, title: 'Details' }
+  { path: 'books/create', component: BookCreatePage, title: 'Buch erstellen' },
+  { path: 'books/:isbn', component: BookDetailsPage, title: 'Details' },
 ];
